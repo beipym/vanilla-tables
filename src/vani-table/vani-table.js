dbf4ffb2c042bac05f4fedf55e0c9fd9f5adbf90
beipym <payamb.wrk@gmail.com>
2023-12-03 09:07:19 +0330

    a function to create table

---
+++ src/vani-table/vani-table.js
@@ -0,0 +1,77 @@
+import {getTableColumns} from "./table-columns";
+
+
+// TODO : get columns list as object 
+export function createTable(containerId){
+
+    // safety check
+    if(!containerId){
+        throw new Error("no table container id provided!")
+    }
+    const tableContainerRef = document.querySelector(`#${containerId}`)
+    if(!tableContainerRef){
+        throw new Error("table container not found")
+    }
+
+    // create table and append to the container
+    const table = document.createElement('table');
+    table.classList = [
+        'w-full',
+        'p-8',
+    ].join(' ');
+    
+    
+    // initialize column headers
+    const headerRow = document.createElement('tr')
+    getTableColumns().map(element => {
+        const th = document.createElement('th');
+        th.innerHTML = element.title;
+        th.setAttribute('data-column-header', element.column);
+        th.classList = [
+            'border'
+        ].join(" ");
+        headerRow.appendChild(th);
+    });
+    headerRow.classList = [
+        'bg-slate-100',
+        'py-4'
+    ].join(' ');
+    table.appendChild(headerRow)
+    
+    tableContainerRef.appendChild(table)
+
+}
+
+export function populateTable(refId,data){
+    const tableRef = document.querySelector(`#${refId}`)
+    let tbody = tableRef.querySelector('tbody');
+    
+    if(tbody){
+        tbody.innerHTML = '';
+    } else {
+        tbody = document.createElement('tbody');
+    }
+
+    // TODO : Check this !
+    const pageData = data;
+    const fragment = new DocumentFragment();
+    
+    pageData.forEach(row => {
+        const tr = document.createElement('tr');
+        Object.keys(row).forEach(cell => {
+            if(cell !== 'id'){
+
+                const td = document.createElement('td');
+                td.textContent = row[cell];
+                tr.appendChild(td);
+                td.classList = [
+                    'border',
+                    'px-4'
+                ].join(' ')
+            }
+        });
+        fragment.appendChild(tr);
+    });
+    tbody.appendChild(fragment);
+    tableRef.appendChild(tbody)
+}
