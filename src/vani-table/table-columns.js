03510d11352398d6c7a9d20b358e67fe0e352375
beipym <payamb.wrk@gmail.com>
2023-12-03 09:14:00 +0330

    keeping the table columns in a separate file

---
+++ src/vani-table/table-columns.js
@@ -0,0 +1,10 @@
+const tableColumns = [
+    {title:'Name' , column:'name'},
+    {title:'Date' , column:"date"},
+    {title:'Title', column:'title'},
+    {title:'field', column:'field'},
+    {title:'Old Value', column:'old_value'},
+    {title:'New Value', column:'new_value'},
+]
+
+export const getTableColumns = () => tableColumns
